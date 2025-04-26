cd1dc95330dbe52d946e61a7dc156ed90e410045
ZODIAC <abraar237@gmail.com>
2025-04-26 19:07:36 +0530

    ok

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// src/App.tsx
+import React from "react";
+import ScreenShare from "./components/ScreenShare";
+import { WebSocketProvider } from "./components/WebSocketProvider";
+
+const App: React.FC = () => {
+  return (
+
+    // <WebSocketProvider url="ws://0.0.0.0:9084">
+<WebSocketProvider url="wss://rein-backend-iept.onrender.com">
+      <div className="container mx-auto p-4">
+        <ScreenShare />
+      </div>
+    </WebSocketProvider>
+  );
+};
+
+export default App;
